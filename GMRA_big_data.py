58e4f5385960b8c94b4924cccdfacf43549a33f1
tmarino2 <tmarino2@jhu.edu>
2016-12-03 00:49:50 -0500

    Add files via upload

--- GMRA_big_data.py
+++ GMRA_big_data.py
@@ -71,7 +71,7 @@ class C_jk:
         #C_jk_centered = mean_zip.map(lambda (row,m): np.asarray(map(lambda row_el: row_el - m, row)))
         c_jk = np.copy(self.c_jk)#fucking python lazy evaluation
         C_jk_centered = self.C_jk.map(lambda row: row - c_jk)
-        C_jk_centered.first()#fucking python lazy evaluation
+        #C_jk_centered.first()#fucking python lazy evaluation
         rowMatr = RowMatrix(C_jk_centered)
         svd = computeSVD(rowMatr,self.dim)
         self.P_jk = svd.V.toArray()
@@ -84,3 +84,80 @@ class C_jk:
         C_jk_projected = self.C_jk.map(lambda point: np.transpose(P_jk).dot(point))
         self.C_jk_projected = C_jk_projected
         return C_jk_projected
+    
+class GMRA:
+    
+    def __init__(self, data, manifold_dim, resolution):
+        #self.data = np.transpose(data) #since our data is in rows
+        self.data = data
+        self.dim = manifold_dim
+        self.res = resolution
+        self.low_dim_reps = []
+        self.resolutions = []
+        
+    def split_step(self):
+        rdd = self.data
+        #take an rdd with row-wise data and split it in two clusters with k-means
+        #return the two rdd's wrt to the clusters
+        kmeans_b = KMeans.train(rdd, 2, maxIterations=10, seed=50, initializationSteps=5)
+        centers = kmeans_b.predict(rdd)
+        to_split = rdd.zip(centers)
+        #probably a better way to do this
+        cluster_0 = to_split.filter(lambda (entry,cluster) : cluster == 0)
+        cluster_1 = to_split.filter(lambda (entry,cluster) : cluster == 1)
+        return cluster_0.map(lambda (entry,cluster) : entry), cluster_1.map(lambda (entry,cluster) : entry)
+    
+    def project_cluster(self, data):
+        C_jk = C_jk(data,self.dim)
+        low_dim_rep = C_jk.project()
+        return C_jk.c_jk, C_jk.P_jk, low_dim_rep
+    
+    def fit(self, data=None, dim=None, res=None):
+        if data==None:
+            data = self.data
+        if dim==None:
+            dim = self.dim
+        if res==None:
+            res = self.res
+        c_jk, Phi_jk, low_dim_rep = self.project_cluster(data)
+        resolutions = [(data,c_jk,Phi_jk)]
+        low_dim_reps = [low_dim_rep]
+        for j in xrange(2**res+1):
+            print "At "+str(j)
+            if resolutions[j] != None and resolutions[j][0].count()>1:
+                cluster_0,cluster_1 = self.split_step(resolutions[j][0])
+                c_jk0, Phi_jk0, low_dim_rep_k0 = self.project_cluster(cluster_0)
+                c_jk1, Phi_jk1, low_dim_rep_k1 = self.project_cluster(cluster_1)
+                if self.subsp_angle(Phi_jk0,Phi_jk1) < 0.99999: #replace by epsilon of choice
+                    resolutions += [(rep_k0,c_jk0,Phi_jk0),(rep_k1,c_jk1,Phi_jk1)]
+                    low_dim_reps += [low_dim_rep_k0,low_dim_rep_k1]
+                else:
+                    resolutions += [None]
+                    low_dim_reps += [None]
+        self.low_dim_reps = low_dim_reps
+        self.resolutions = resolutions
+        return low_dim_reps,resolutions
+    
+    def project_test_point(self, point):
+        point_reps = [np.transpose(self.resolutions[0][2]).dot(point)]
+        j = 0
+        while 2**j+1 < len(self.resolutions):
+            if self.resolutions[j] == None:
+                break
+            idx1 = 2**j+1
+            idx2 = 2**j+2
+            dist1 = np.linalg.norm(self.resolutions[idx1][1]-point)
+            dist2 = np.linalg.norm(self.resolutions[idx2][1]-point)
+            if dist1 <= dist2:
+                point_reps.append(np.transpose(self.resolutions[idx1][2]).dot(point-self.resolutions[idx1][1]))
+                j = idx1
+            else:
+                point_reps.append(np.transpose(self.resolutions[idx2][2]).dot(point-self.resolutions[idx1][1]))
+                j = idx2
+        return point_reps
+    
+    def subsp_angle(self, A, B):
+        ab = np.trace(np.transpose(A).dot(B))
+        a = np.sqrt(np.trace(np.transpose(A).dot(A)))
+        b = np.sqrt(np.trace(np.transpose(B).dot(B)))
+        return ab/(a*b)
